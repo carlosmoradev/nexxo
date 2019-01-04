4aeb8703f1abaca0c2edc817ac8d26b47ade35f2
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2019-01-04 14:54:25 -0500

    validando que el archivo del listado de conexiones ya este disponible

--- nexxo.sh
+++ nexxo.sh
@@ -7,8 +7,18 @@
 CENTRAL="server"
 SERVER=$(cat "$CENTRAL")
 
-ssh root@$SERVER ls /tmp/ |grep openvpn > /tmp/vpn
+if ls /tmp/vpn
+then
 
 DEST=$1
-
 for i in $(cat /tmp/vpn); do ssh root@$SERVER cat /tmp/$i/tmp/gerencia-status.log |grep -i $DEST; done
+
+else
+
+	echo "Estableciendo la primera conexion"
+
+ssh root@$SERVER ls /tmp/ |grep openvpn > /tmp/vpn
+
+
+fi
+
